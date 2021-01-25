f8f338f1fd86d16c0f31282daa41fdd451b228d5
paulreitz <avenose@gmail.com>
2021-01-25 09:56:57 -0800

    Added user registration to the database

--- sql/database20200125.sql
+++ sql/database20200125.sql
@@ -29,3 +29,29 @@ CREATE TYPE auth_action_failure AS (
     success boolean,
     message text
 );
+
+CREATE or replace function createUser(userName varchar(256), userPass text, activationKey varchar(100))
+returns json as $createUser$
+declare
+    result json;
+    createdAt date = now();
+    userKey uuid = uuid_generate_v4();
+    foundUser record;
+    foundCode record;
+begin
+    select * from users into foundUser where name=userName or user_key=userKey;
+    if not found then
+        select * from activation_codes into foundCode where activation_key=activationKey and valid=true;
+        if not found then
+            result = row_to_json(row(false, 'Invalid activation code')::auth_action_failure);
+        else
+            insert into users (user_key, name, display_name, avatar, created_at, password) VALUES (userKey, userName, userName, 'default.jpg', createdAt, crypt(userPass, gen_salt('bf')));
+            update activation_codes set valid=false where activation_key=activation_key;
+            result = row_to_json(row(true, userName, userName, 'default.jpg', userKey, createdAt)::auth_action_success);
+        end if;
+    else
+        result = row_to_json(row(false, 'Username already in use')::auth_action_failure);
+    end if;
+    return result;
+end;
+$createUser$ language plpgsql;
