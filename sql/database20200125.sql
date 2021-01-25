2b9698d704ecb4322baae1f94337b4d4a1cd4c94
paulreitz <avenose@gmail.com>
2021-01-25 09:19:38 -0800

    Added tables to database.

--- sql/database20200125.sql
+++ sql/database20200125.sql
@@ -1,2 +1,16 @@
 CREATE EXTENSION pgcrypto;
 CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    user_key uuid unique,
+    name varchar(256) unique,
+    avatar varchar(256),
+    created_at date,
+    password text
+);
+
+CREATE TABLE activation_codes (
+    activation_key varchar(100) unique,
+    valid boolean default true
+);
