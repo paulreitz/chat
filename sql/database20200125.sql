76775e3ea3b22314a5d42b5e0594cd6a60afcd92
paulreitz <avenose@gmail.com>
2021-01-25 09:42:36 -0800

    Added display name to user table in database

--- sql/database20200125.sql
+++ sql/database20200125.sql
@@ -5,6 +5,7 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     user_key uuid unique,
     name varchar(256) unique,
+    display_name varchar(256),
     avatar varchar(256),
     created_at date,
     password text
@@ -13,4 +14,18 @@ CREATE TABLE users (
 CREATE TABLE activation_codes (
     activation_key varchar(100) unique,
     valid boolean default true
+);
+
+CREATE TYPE auth_action_success AS (
+    success boolean,
+    name varchar(256),
+    display_name varchar(256),
+    avatar varchar(256),
+    user_key uuid,
+    created_at date
+);
+
+CREATE TYPE auth_action_failure AS (
+    success boolean,
+    message text
 );
