c882f58662e117d4601351229b8bfe807182367e
paulreitz <avenose@gmail.com>
2021-01-26 10:54:29 -0800

    Added the ability to update the user display name in the API

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
         router.route(`${path}/create`).post(this.create);
         router.route(`${path}/auth`).post(this.authenticate);
         router.route(`${path}/avatar`).post(this.updateAvatar);
+        router.route(`${path}/display`).post(this.updateDisplayName);
     }
 
     create = (req, res) => {
@@ -55,11 +56,42 @@ class UserController {
             if (req.body && req.body.avatar) {
                 this.userDB.updateAvatar(key, req.body.avatar)
                 .then(result => {
-                    res.status(200).send(result);
+                    if (result.success) {
+                        res.status(200).send(result);
+                    }
+                    else {
+                        res.status(403).send(result);
+                    }
                 })
                 .catch(error => {
                     res.status(500).send({error});
                 }); 
+            }
+            else {
+                res.status(400).send({success: false, message: 'Missing Parameters'});
+            }
+        }
+        else {
+            res.status(403).send({success: false, message: 'Unauthorized'});
+        }
+    }
+
+    updateDisplayName = (req, res) => {
+        const key = this.verifyToken(req.get('x-access-token'));
+        if (key) {
+            if (req.body && req.body.display) {
+                this.userDB.updateDisplayName(key, req.body.display)
+                .then(result => {
+                    if (result.success) {
+                        res.status(200).send(result);
+                    }
+                    else {
+                        res.status(403).send(result);
+                    }
+                })
+                .catch(error => {
+                    res.status(500).send({error});
+                });
             }
             else {
                 res.status(400).send({success: false, message: 'Missing Parameters'});
