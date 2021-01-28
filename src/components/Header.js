f244501347f15c607e306e065b05814a41eeb071
paulreitz <avenose@gmail.com>
2021-01-28 07:43:17 -0800

    Setup the basic header. No navigation added yet, as that will only be available to someone logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
 
-export default class Header extends React.Component {
-    render() {
-        return (
-            <div>Header</div>
-        )
+const useStyles = makeStyles(theme => ({
+    toolbarMargin: {
+        ...theme.mixins.toolbar
     }
-}
+}))
+
+export function Header(props) {
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h5" color="secondary">Basic Chat Application</Typography>
+                </Toolbar>
+            </AppBar>
+            <div className={classes.toolbarMargin}></div>
+        </React.Fragment>
+    )
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: !!state.user.token,
+    displayName: state.user.displayName
+});
+
+export default connect(mapStateToProps)(Header);
